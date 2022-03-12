a9b79424da3dd98a95ceab34634d47fcc9400710
asengin <worksmail2017@gmail.com>
2022-03-12 11:07:25 +0300

    Шрифт и размер шрифта вынесен в ресурсы

--- EasyTextEditor/MainWindow.xaml.cs
+++ EasyTextEditor/MainWindow.xaml.cs
@@ -25,18 +25,19 @@ namespace EasyTextEditor
         public MainWindow()
         {
             InitializeComponent();
+            textBox.Focus();
         }
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            string fontName = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+            string fontName = ((sender as ComboBox).SelectedItem as string);
             if (textBox != null)
                 textBox.FontFamily = new FontFamily(fontName);
         }
 
         private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
         {
-            double fontSize = Convert.ToDouble(((sender as ComboBox).SelectedItem as TextBlock).Text);
+            double fontSize = Convert.ToDouble(((sender as ComboBox).SelectedItem));
             if (textBox != null)
                 textBox.FontSize = fontSize;
         }
